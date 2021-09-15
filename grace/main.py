d9701b583b5f3f8575afd3884ed15a9e757145bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-14 21:20:47 -0300

    Saved grace/main.py @2021-09-15 00:20

--- grace/main.py
+++ grace/main.py
@@ -1,7 +1,1 @@
 # sophie.grace.main.py
-from _spy.vitollino.main import Cena, STYLE
-STYLE["width"], STYLE["height"]= 1350, "600px"
-RIO = "https://i.imgur.com/wz5XKcr.jpg"
-VADER = "https://i.imgur.com/dmRivpB.png"
-praia = Cena(RIO).vai()
-Elemento(VADER, cena=praia)

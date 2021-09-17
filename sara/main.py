add4d046a68964188040915d7cc485596e2b72a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 14:46:46 -0300

    Saved sara/main.py @2021-09-17 17:46

--- sara/main.py
+++ sara/main.py
@@ -1,3 +1,4 @@
 # sophie.sara.main.py/ana clara agatha antonio 
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import IVENTARIO as inv
+CASA https://resizedimgs.zapimoveis.com.br/crop/272x224/vr.images.sp/54dfc9f4a92cc0f90b4f737988369abc.jpg

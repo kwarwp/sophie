7a1f5631d01022e697c48dcd5a6ed9366f0f58d0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 14:46:42 -0300

    Saved stacy/main.py @2021-09-17 17:46

--- stacy/main.py
+++ stacy/main.py
@@ -1,3 +1,4 @@
 # sophie.stacy.main.py/adrielly.jose myguel
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVENTARIO as inv
+CASA = "https://img.freepik.com/vetores-gratis/bela-casa_24877-50819.jpg?size=626&ext=jpg"

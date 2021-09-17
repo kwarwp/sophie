14bdf64193712b9a3a05364d813ca160182b0428
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:05:36 -0300

    Saved roxanne/main.py @2021-09-17 18:05

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,3 +2,4 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVETARIO as inv
 CASA= "https://e7.pngegg.com/pngimages/789/105/png-clipart-house-house.png"
+SAITAMINHA= "https://www.disneyria.com.br/wp-content/uploads/2019/12/olaf-alivio-comico.jpg"

ca8a63678771e79f9739f15be098fa4c75959f94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:20:04 -0300

    Saved sara/main.py @2021-09-17 18:20

--- sara/main.py
+++ sara/main.py
@@ -3,4 +3,4 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import IVENTARIO as inv
 CASA= "https://resizedimgs.zapimoveis.com.br/crop/272x224/vr.images.sp/54dfc9f4a92cc0f90b4f737988369abc.
 BART= "https://minilua.com/wp-content/uploads/2011/05/cartoons011_thumb.jpg"
-class bart simpson ( ):
+class bartsimpson ( ):

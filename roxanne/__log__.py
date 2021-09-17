36c622ca7e6f6d94c1b640cdeefc50d380fa467c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:34:28 -0300

    Created roxanne/__log__.py @2021-09-17 18:34

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Sep 17 2021 15:38:12.790 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  casa=Cena(img=CASA)
+  ^
+IndentationError: expected an indented block
+'''},

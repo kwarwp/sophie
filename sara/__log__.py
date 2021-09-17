c500ddb28d26df7f5a9fd2d55afa686d8db7d48f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:41:48 -0300

    Created sara/__log__.py @2021-09-17 18:41

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Sep 17 2021 08:41:46.375 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  CASA= "https://resizedimgs.zapimoveis.com.br/crop/272x224/vr.images.sp/54dfc9f4a92cc0f90b4f737988369abc.
+                                                                                                          ^
+SyntaxError: EOL while scanning string literal
+'''},

da599e8961eeddb9d819e6d03995e170841585e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:41:59 -0300

    Saved kellee/__log__.py @2021-09-17 18:41

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                            ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Fri Sep 17 2021 15:42:04.246 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  HINATA="https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-mang
+                                                                                                                           ^
+SyntaxError: EOL while scanning string literal
+'''},

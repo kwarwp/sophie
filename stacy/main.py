f28836febaf6b028e879929ce4e4a159ec5efcc3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:19:40 -0300

    Saved stacy/main.py @2021-09-17 18:19

--- stacy/main.py
+++ stacy/main.py
@@ -3,3 +3,4 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVENTARIO as inv
 CASA = "https://img.freepik.com/vetores-gratis/bela-casa_24877-50819.jpg?size=626&ext=jpg"
 STHICH="https://e7.pngegg.com/pngimages/620/826/png-clipart-lilo-stitch-lilo-pelekai-sticker-paper-others-sticker-fictional-character.png"
+class  HOTDOG():

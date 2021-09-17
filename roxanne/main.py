d8a5902aabf2034995a532ac8d7846d89ec6abd0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 14:46:43 -0300

    Saved roxanne/main.py @2021-09-17 17:46

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,3 +1,4 @@
 # sophie.roxanne.main.py/isaque kaicke cleyton/
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVETARIO as inv
+CASA= "https://e7.pngegg.com/pngimages/789/105/png-clipart-house-house.png"

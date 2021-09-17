25e9c1c1c976f24179e5b81a436fc39fa236d7e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:19:31 -0300

    Saved kellee/main.py @2021-09-17 18:19

--- kellee/main.py
+++ kellee/main.py
@@ -2,4 +2,7 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVETARIO as inv
 CASA ="https://imagensemoldes.com.br/wp-content/uploads/2020/05/Condom%C3%ADnio-Casa-PNG-1280x720.png"
-HINATA="https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-manga.jpg"
+HINATA="https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-mang
+class flojinha():
+
+

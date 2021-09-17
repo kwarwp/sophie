3082c8b304d0177b5555c40db1401e26c2b3282c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-17 15:36:33 -0300

    Saved roxanne/main.py @2021-09-17 18:36

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,10 +1,10 @@
 # sophie.roxanne.main.py/isaque kaicke cleyton/
 from _spy.vitollino.main import Cena,Elemento,Texto
-from _spy.vitollino.main import INVETARIO as inv
+#from _spy.vitollino.main import INVETARIO as inv
 CASA= "https://e7.pngegg.com/pngimages/789/105/png-clipart-house-house.png"
 SAITAMINHA= "https://www.disneyria.com.br/wp-content/uploads/2019/12/olaf-alivio-comico.jpg"
 class kaka():
-casa=Cena(img=CASA)
-saitaminha=Elemento(img=SAITAMINHA)
-casa.vai()
+	casa=Cena(img=CASA)
+	saitaminha=Elemento(img=SAITAMINHA)
+	casa.vai()
 saitaminha()

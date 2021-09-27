d2a5bcc3196c7a1bc7914c412cce74b205a8358a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-09-27 15:27:43 -0300

    Saved stacy/__log__.py @2021-09-27 18:27

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -17,3 +17,21 @@
     	self.sthitch.entrar(self.casa)
 AttributeError: 'HOTDOG' object has no attribute 'sthitch'
 '''},
+{'date': 'Mon Sep 27 2021 08:27:42.264 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    HOTDOG()
+  module <module> line 10
+    	self.sthich.entrar(self.casa)
+AttributeError: 'Elemento' object has no attribute 'entrar'
+'''},

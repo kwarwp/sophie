a85a2481fed5b36fad07cfc67fb30012d847bf80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 15:30:48 -0300

    Saved roxanne/main.py @2021-10-04 18:30

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,12 +4,14 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 CASA= "https://e7.pngegg.com/pngimages/789/105/png-clipart-house-house.png"
 SAITAMINHA= "https://www.disneyria.com.br/wp-content/uploads/2019/12/olaf-alivio-comico.jpg"
 YORK= "https://www.bigdream.com.br/_uploads/05bc1762-649c-44aa-8634-b992f998852d_Zoom.jpg"
-furia= "https://e7.pngegg.com/pngimages/816/446/png-clipart-hiccup-horrendous-haddock-iii-how-to-train-your-dragon-astrid-toothless-others-dragon-fictional-character.png"
+FURIA= "https://e7.pngegg.com/pngimages/816/446/png-clipart-hiccup-horrendous-haddock-iii-how-to-train-your-dragon-astrid-toothless-others-dragon-fictional-character.png"
 
 class kaka():
     def __init__ (self):
 	self.casa=Cena(img=CASA)
 	self.saitaminha=Elemento(img=SAITAMINHA)
+	self.york=Cena(img=YORK)
+	self.furia=Elemento(img=FURIA)
 	self.saitaminha.entra(self.casa)
 	self.casa.vai()
 kaka()

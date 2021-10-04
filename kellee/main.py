5442ac3b714f49903fbc262a940d0d475a662ac0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 14:30:12 -0300

    Saved kellee/main.py @2021-10-04 17:30

--- kellee/main.py
+++ kellee/main.py
@@ -1,10 +1,11 @@
-# sophie.kellee.main.py/joao keyla beatriz/ 
+# sophie.kellee.main.py/joao stefhanny alves/ 
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVETARIO as inv
 CASA ="https://imagensemoldes.com.br/wp-content/uploads/2020/05/Condom%C3%ADnio-Casa-PNG-1280x720.png"
-HINATA="https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-mang https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-manga.jpg
+PARIS="https://cdn.getyourguide.com/img/location/5ffeb39946966.jpeg/88.jpg"
+barbara="https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-barbara-uzumaki-mang https://besthqwallpapers.com/Uploads/25-2-2019/81790/thumb2-hinata-hyuga-4k-naruto-characters-hinata-uzumaki-manga.jpg
 class flojinha():
 	casa=Cena(img=CASA)
-	personagen=Elemento(img=HINATA)
+	personagen=Elemento(img=barbara)
 	casa.vai()
 flojinha()

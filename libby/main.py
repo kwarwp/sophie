c789d91b97d4f6edce02da7aa633c7cc84c02ca6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 14:51:03 -0300

    Saved libby/main.py @2021-10-04 17:51

--- libby/main.py
+++ libby/main.py
@@ -1,7 +1,7 @@
 # sophie.libby.main.py cleyton jhonnison 
 from _spy.vitollino.main import cena,elemento
 from _spy.vitollino.main import invetario as inv
-MANSAO="jpg ou png"
+MANSAO="https://i.pinimg.com/736x/3d/74/b1/3d74b17105ce1d2046935a29331cc55e.jpg "
 CLARK="jpg ou png"
 class KING ():
     def __init__(self): 

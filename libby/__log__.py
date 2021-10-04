4b9d811f0bccd9ef582caf452263ec2b79a65057
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 15:26:57 -0300

    Saved libby/__log__.py @2021-10-04 18:26

--- libby/__log__.py
+++ libby/__log__.py
@@ -61,3 +61,21 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy.vitollino.main import invetario as inv
 '''},
+{'date': 'Mon Oct 04 2021 08:26:56.115 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    OLAF()
+  module <module> line 8
+    self.masao=cena(img=MASAO)
+NameError: name 'cena' is not defined
+'''},

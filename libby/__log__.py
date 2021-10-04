02b699acd9e8d897a0a03ba9148803e0f283612b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 15:27:46 -0300

    Saved libby/__log__.py @2021-10-04 18:27

--- libby/__log__.py
+++ libby/__log__.py
@@ -97,3 +97,21 @@ NameError: name 'cena' is not defined
     self.masao=Cena(img=MASAO)
 NameError: name 'MASAO' is not defined
 '''},
+{'date': 'Mon Oct 04 2021 08:27:45.443 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    OLAF()
+  module <module> line 11
+    self.masao.vai()
+AttributeError: 'OLAF' object has no attribute 'masao'
+'''},

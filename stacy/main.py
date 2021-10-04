3160e30eec7a6da3a04dae861d836f5c7fdfeca6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 14:46:20 -0300

    Saved stacy/main.py @2021-10-04 17:46

--- stacy/main.py
+++ stacy/main.py
@@ -2,8 +2,9 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVENTARIO as inv
 CASA = "https://img.freepik.com/vetores-gratis/bela-casa_24877-50819.jpg?size=626&ext=jpg"
-STHICH="https://e7.pngegg.com/pngimages/620/826/png-clipart-lilo-stitch-lilo-pelekai-sticker-paper-others-sticker-fictional-character.png"
-class  HOTDOG():   
+STHICH="https://e7.pngegg.com/pngimages/620/826/png-clipart-lilo-stitc
+f36resta="https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2020/03/04/832145/20200304201404477200e.jpg
+*assar6="https://s3.amazonaws.com/is-ebird-wordpress-prod-s3/wp-content/uploads/sites/55/Screen-Shot-2015-06-04-at-8.51.39-AM.png
     def __init__(self):
 	self.casa=Cena(img=CASA)
 	self.sthich =Elemento(img=STHICH)

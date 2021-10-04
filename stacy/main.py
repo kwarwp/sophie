26094a22d9098a0442caec45e8eb91f7a779ab51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 14:53:29 -0300

    Saved stacy/main.py @2021-10-04 17:53

--- stacy/main.py
+++ stacy/main.py
@@ -1,7 +1,7 @@
 # sophie.stacy.main.py/adrielly.jose myguel
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import INVENTARIO as inv
-CASA = "https://img.freepik.com/vetores-gratis/bela-casa_24877-50819.jpg?size=626&ex
+CASte36="https://img.elo7.com.br/product/original/2F882F8/fundo-fotografico-em-tecido-castelo-2-20x1-50m-backdrop.jpg
 fr6zen="https://i.ytimg.com/vi/gIOyB9ZXn8s/maxresdefault.jpg
 f36resta="https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2020/03/04/832145/20200304201404477200e.jpg
 *assar6="https://s3.amazonaws.com/is-ebird-wordpress-prod-s3/wp-content/uploads/sites/55/Screen-Shot-2015-06-04-at-8.51.39-AM.png

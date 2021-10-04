db26158c52b3ebe35f970755ca048dbc01118ae3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 13:43:48 -0300

    Saved roxanne/__log__.py @2021-10-04 16:43

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -203,3 +203,21 @@ TypeError: 'module' object is not callable
     	self.casa=Cena(img=CASA)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Mon Oct 04 2021 13:43:51.415 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    kaka()
+  module <module> line 8
+    	self.casa=Cena(img=CASA)
+TypeError: 'module' object is not callable
+'''},

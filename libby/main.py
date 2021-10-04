a6e6a4bb191e6d1c914cfabb122741ea2213da51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-04 15:28:29 -0300

    Saved libby/main.py @2021-10-04 18:28

--- libby/main.py
+++ libby/main.py
@@ -1,14 +1,14 @@
 # sophie.libby.main.py cleyton jhonnison 
-from _spy.vitollino.main import cena,elemento
-from _spy.vitollino.main import invetario as inv
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
 MANSAO="https://i.pinimg.com/736x/3d/74/b1/3d74b17105ce1d2046935a29331cc55e.jpg "
-CLARK="jpg ou png"
-class KING ():
+CLARK="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Imagem-Olaf-Cenouras-Frozen-Frozen-PNG-1280x720.png"
+class OLAF ():
     def __init__(self): 
-        self.MASAO=cena(img=MASAO)
-        self.clark=elemento(img=CLARK
-        self.casa.vai()
-        self.MANSAO.vai()
-KING()
+        self.mansao=Cena(img=MANSAO)
+        self.clark=Elemento(img=CLARK)
+        self.clark.entra(self.mansao)
+        self.mansao.vai()
+OLAF()
         
         

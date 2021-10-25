ca1dd1198f44e88132f23d2894abb99fee5fa49f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-25 14:29:28 -0300

    Saved sara/main.py @2021-10-25 17:29

--- sara/main.py
+++ sara/main.py
@@ -1,6 +1,6 @@
 # sophie.sara.main.py/ana clara kaicke
 from _spy.vitollino.main import Cena,Elemento,Texto
-from _spy.vitollino.main import IVENTARIO as inv
+from _spy.vitollino.main import IVENTARIO as INV
 CASA= "https://resizedimgs.zapimoveis.com.br/crop/272x224/vr.images.sp/54dfc9f4a92cc0f90b4f737988369abc.jpb"
 BART= "https://minilua.com/wp-content/uploads/2011/05/cartoons011_thumb.jpg"
 PREDIO= "https://i.pinimg.com/originals/ff/30/4e/ff304e7727b7782d04e0eecb5b238eb8.jpg"

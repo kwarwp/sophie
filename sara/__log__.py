a19f3f1324b5f18ff3f682eb07abcb34ee85d42d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-25 14:28:32 -0300

    Saved sara/__log__.py @2021-10-25 17:28

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,34 @@
                                                                                                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Oct 25 2021 07:28:31.140 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from _spy.vitollino.main import IVENTARIO as inv
+ImportError: cannot import name 'IVENTARIO'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from _spy.vitollino.main import IVENTARIO as inv
+'''},

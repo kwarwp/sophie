7e2085d3a7734b93e84420fbe5f4ff72312f5c2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-25 14:34:09 -0300

    Saved sara/__log__.py @2021-10-25 17:34

--- sara/__log__.py
+++ sara/__log__.py
@@ -186,3 +186,21 @@ Traceback (most recent call last):
     	self.casa.direita=self.casa
 AttributeError: 'bartsimpson' object has no attribute 'casa'
 '''},
+{'date': 'Mon Oct 25 2021 07:34:07.880 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    bartsimpson()
+  module <module> line 18
+    	casa.vai()
+NameError: name 'casa' is not defined
+'''},

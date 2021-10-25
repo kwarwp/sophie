8cecd303ddd552529fa04784a0a9ad2539db016a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-25 14:39:59 -0300

    Saved sara/main.py @2021-10-25 17:39

--- sara/main.py
+++ sara/main.py
@@ -1,7 +1,7 @@
 # sophie.sara.main.py/ana clara kaicke
 from _spy.vitollino.main import Cena,Elemento,Texto
-#from _spy.vitollino.main import INVENTARIO  as inv
-CASA= "https://resizedimgs.zapimoveis.com.br/crop/272x224/vr.images.sp/54dfc9f4a92cc0f90b4f737988369abc.jpb"
+from _spy.vitollino.main import INVENTARIO  as inv
+CASA= "https://www.plantapronta.com.br/projetos/140/01.jpg"
 BART= "https://minilua.com/wp-content/uploads/2011/05/cartoons011_thumb.jpg"
 PREDIO= "https://i.pinimg.com/originals/ff/30/4e/ff304e7727b7782d04e0eecb5b238eb8.jpg"
 WANDINHA="https://images.uncyc.org/pt/thumb/9/9a/Familia-addams-2-05.jpg/300px-Familia-addams-2-05.jpg"

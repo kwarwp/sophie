4104b3d0e674c2f43c978e2909a9e186c4d1cd02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-11-08 15:21:19 -0300

    Created danae/main.py @2021-11-08 18:21

---
+++ danae/main.py
@@ -0,0 +1,5 @@
+# sophie.danae.main.py
+CLARK="https://p4.wallpaperbetter.com/wallpaper/535/519/761/aqua-eyes-armor-blond-hair-cape-wallpaper-preview.jpg"
+MANSAO="https://i.pinimg.com/originals/43/8c/c8/438cc85a480c66135108fb1773fcc8ef.png"
+FAMILIA="https://upload.wikimedia.org/wikipedia/pt/e/e9/Personagens_de_DB.jpg"
+REINADO="https://image.freepik.com/vetores-gratis/cartaz-dos-desenhos-animados-do-castelo-com-o-rei-sentado-no-trono-rodeado-por-sua-comitiva-e-cidadao-pedindo-ilustracao-de-joelhos_1284-29057.jpg

1477db96a709b8ad7c520cdb41d9a8b0875291f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-11-08 15:26:42 -0300

    Saved amanda/main.py @2021-11-08 18:26

--- amanda/main.py
+++ amanda/main.py
@@ -1,3 +1,3 @@
-# sophie.amanda.main.py
-https://img2.gratispng.com/20180628/ysx/kisspn765120315301983895143.jpg
-https://img.lovepik.com/photo/50035/1532.jpg_wh860.jpg
+# sophie.amanda.main.py Agatha & isaque 
+HARRYPOTTER="https://i.smalljoys.me/2018/07/harrypotter.png"
+CASTELOREAL="https://img.lovepik.com/photo/50035/1532.jpg_wh860.jpg"

ab5419496fa7ef61ba2b844799f5cdc0d481e66c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-11-08 15:21:24 -0300

    Created danae/__log__.py @2021-11-08 18:21

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 08 2021 08:21:20.589 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  REINADO="https://image.freepik.com/vetores-gratis/cartaz-dos-desenhos-animados-do-castelo-com-o-rei-sentado-no-trono-rodeado-por-sua-comitiva-e-cidadao-pedindo-ilustracao-de-joelhos_1284-29057.jpg
+                                                                                                                                                                                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
